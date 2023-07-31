19dbbcd0501c21712c8ceb67b176e8f327384e97
jiangwei <jiangwei@tejin.com>
2023-07-31 10:06:27 +0800

    feat:新增图片等模块

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import image from "@astrojs/image";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jwnoal.github.io',
-	integrations: [mdx(), sitemap()],
+  site: 'https://jwnoal.github.io',
+  integrations: [mdx(), sitemap(), image(), tailwind()]
 });
