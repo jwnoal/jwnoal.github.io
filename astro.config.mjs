178981ec9040faf90a403017341dc5cfe49fd266
jwnoal <jwnoal@163.com>
2023-10-14 15:09:16 +0800

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+// import vercel from "@astrojs/vercel/serverless";
 
-import vercel from "@astrojs/vercel/serverless";
+import vue from "@astrojs/vue";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-  output: "server",
-  adapter: vercel()
+  integrations: [mdx(), sitemap(), vue(), react({
+    include: ['**/react/*'],
+  })]
+  // output: "server",
+  // adapter: vercel()
 });
