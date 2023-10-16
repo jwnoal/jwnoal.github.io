0e37808082804e8b00bf629c6df44979dc55eafe
jwnoal <jwnoal@163.com>
2023-10-16 18:17:26 +0800

    优化

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,14 +15,7 @@ export default defineConfig({
   adapter: vercel(),
   markdown: {
     shikiConfig: {
-      // 选择 Shiki 内置的主题（或添加你自己的主题）
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       theme: 'one-dark-pro',
-      // 添加自定义语言
-      // 注意：Shiki 内置了无数语言，包括 .astro！
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: [],
-      // 启用自动换行，以防止水平滚动
       wrap: true,
     },
   },
