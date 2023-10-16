ad77ab114cd2a65366baa89bec4b670208d4ff2d
jwnoal <jwnoal@163.com>
2023-10-16 20:56:41 +0800

    hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [mdx(), sitemap(), vue(), react({
     include: ['**/react/*'],
   })],
-  output: 'server',
+  output: 'hybrid',
   adapter: vercel(),
   markdown: {
     shikiConfig: {
