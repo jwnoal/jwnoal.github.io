be4e3dc6766d63cd21870455d934c8ed407c1a07
jwnoal <jwnoal@163.com>
2023-10-16 20:20:15 +0800

    test

--- src/utils/getSiteUrl.ts
+++ src/utils/getSiteUrl.ts
@@ -1,8 +1,9 @@
 const base = import.meta.env.BASE_URL;
 
 export function getSiteURL(
-    url: string | URL,
-    b?: string | URL | undefined
+  url: string | URL,
+  b?: string | URL | undefined
 ): URL {
-    return new URL(`${base === "/" ? "" : base}${url}`, b);
+  // return new URL(`${base === "/" ? "" : base}${url}`, b);
+  return new URL(`${base === "/" ? "" : '/'}${url}`, b);
 }
