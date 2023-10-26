1a85f280f981a9e844ee4637dcca7312755447a1
jwnoal <jwnoal@163.com>
2023-10-26 20:14:47 +0800

    新增mdx博客

--- src/components/vue/Demo.vue
+++ src/components/vue/Demo.vue
@@ -1,38 +1,11 @@
 <template>
-  <button @click="assCount">Count is: {{ count }}</button>
-  <div>{{ newCount }}</div>
-  {{ obj }}
-  {{ obj.c }}
-  {{ calculateBooksMessage() }}
+ 
+  <div>
+    <List></List>
+  </div>
 </template>
 
 <script setup>
+import List from "./List.vue";
 import { ref, computed, onMounted, nextTick } from "vue";
-const count = ref(1);
-setTimeout(() => {
-  count.value++;
-}, 0);
-function assCount() {
-  count.value++;
-}
-// 生命周期钩子
-onMounted(() => {
-  console.log("onMounted");
-});
-
-const obj = ref({ a: 1 });
-setTimeout(() => {
-  obj.value.b = 2;
-  obj.value = { c: 1 };
-}, 0);
-
-const newCount = computed(() => {
-  return count.value - 1;
-});
-
-function calculateBooksMessage() {
-  return Date.now();
-}
-
-await nextTick();
 </script>
