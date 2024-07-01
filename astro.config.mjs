ca21449f181db89d3e8c9d4faa3e8311e020876b
jwnoal <jwnoal@163.com>
2024-07-01 16:09:40 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jump.icu/',
+  site: 'https://jiangwei.zone/',
   base: '/',
   trailingSlash: 'always',
   integrations: [
